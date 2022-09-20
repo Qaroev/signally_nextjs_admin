6e499e7bd1edbe2ec80d4757d18a184a25839f61
Bobojon Qaroev <gulboyqaroev@Mail.ru>
2022-09-20 20:21:07 +0500

    commit

--- src/utils/app_constants.ts
+++ src/utils/app_constants.ts
@@ -6,11 +6,11 @@ export const APP_MODE: I_APP_MODE = 'PROD';
 export const APP_NAME = 'TheChurchApp';
 
 export const firebaseConfig = {
-  apiKey: 'XXXXXXXXXXXXXXXXXXXXX',
-  authDomain: 'XXXXXXXXXXXXXXXXXXXXX',
-  projectId: 'XXXXXXXXXXXXXXXXXXXXX',
-  storageBucket: 'XXXXXXXXXXXXXXXXXXXXX',
-  messagingSenderId: 'XXXXXXXXXXXXXXXXXXXXX',
-  appId: 'XXXXXXXXXXXXXXXXXXXXX',
-  measurementId: 'XXXXXXXXXXXXXXXXXXXXX',
+  apiKey: "AIzaSyBhCfxJEE8q_n0GbaTWwTPYEDw8ijgvZQY",
+  authDomain: "signal-eaql.firebaseapp.com",
+  projectId: "signal-eaql",
+  storageBucket: "signal-eaql.appspot.com",
+  messagingSenderId: "727838641216",
+  appId: "1:727838641216:web:e49c6420fbe004a81923a5",
+  measurementId: "G-WG6PTDJ4NC"
 };
